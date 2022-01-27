e23a96d7bda0569e1a1ac75e6d19245dece5cf58
James_G <jamesg.chef@gmail.com>
2022-01-27 17:27:06 +0000

    Initial stages of build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+const MealSelector = (meals) => {
+  console.log(meals)
+    let randomNumber = Math.floor(Math.random() * 7);
+      return meals[randomNumber].name
+    
+}
+  
+
+
+const meals = [
+  {
+    name: "Cheese on toast",
+    convenience: 9,
+  },
+  {
+    name: "Chicken ceasar salad",
+    convenience: 6,
+  },
+  {
+    name: "Pasta Arrabiatta",
+    convenience: 6,
+  },
+  {
+    name: "Risotto",
+    convenience: 6,
+  },
+  {
+    name: "Steak and chips",
+    convenience: 6,
+  },
+  {
+    name: "Falafel and salad",
+    convenience: 6,
+  },
+  {
+    name: "Sunday Roast",
+    convenience: 6,
+  },
+];
+
+const DaysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const MealCard = (props) => {
+  return <p>{props.day}: {props.meal}</p>
+}
+
+const Food = "pizza"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header bestmeal={MealSelector(meals)}/>
+     <MealCard day={DaysOfWeek[2]} meal={meals[3]} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+const Header = (props) => {
+    console.log(props)
+    return <h1>Weekly dinner planner...{props.bestmeal }</h1>
+}
+
+export default Header
